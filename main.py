36c0671d543e4dd0ef206add3689b72fe5f4aae7
BakedBeanLol <benjamin.mead@lphs.school.nz>
2021-03-24 20:45:45 +0000

    testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request # Importing the Flask Library/App
+import requests
 import random  # Importing the random Library
+from discord_webhook import DiscordWebhook
 
 app = Flask( # Creating the flask app
   __name__,
@@ -11,8 +13,17 @@ app = Flask( # Creating the flask app
 def say():  # Function for when a post request is sent
   if request.method == "POST":   
     print("someone sent a post request") # logging "someone sent a post request" in the console
-    print(request.form['stuff']) # !
-    return "aaaaaaa" 
+
+    data = request.form['stuff'] # requesting the data input in the html form and storing it in data variable
+    print(data)
+
+    url = "https://discord.com/api/webhooks/823821827285057576/oS3Y48XrLhjFOQ4H9ZWv4TLtlZoH2V-Lc1hb7clXwOZcPURc_30NHJVJcHDnhMc2dSPG"
+    # requests.post(url, data)
+    webhook = DiscordWebhook(url, data)
+    response = webhook.execute()
+    
+    return "Thank You For Entering Data"  # returning some feedback to the user after they submit the data
+    
 
 @app.route('/') # Defining where our site will run by default
 def home():
