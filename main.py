a5d70bb9c589814e8dcde50a6e0e421e99960c44
BakedBeanLol <benjamin.mead@lphs.school.nz>
2021-03-28 23:15:26 +0000

    few minor changes such as comments and adding in the final form input support

--- main.py
+++ main.py
@@ -11,21 +11,25 @@ app = Flask( # Creating the flask app
 @app.route('/webhook', methods=["POST"]) # Defining where our site will run if someone sends a post request
 def say():  # Function for when a post request is sent
   if request.method == "POST":   
-    print("someone sent a post request") # logging "someone sent a post request" in the console
-    name = request.form['stuff'] # requesting the data input in the html form and storing it in data variable
-    info = request.form['morestuff']
+    print("Someone wants to join your club!") # logging "someone sent a post request" in the console
+    name = request.form['name'] # requesting data input in the form asking for name
+
+    year = request.form['year'] # requesting data input in the form asking for year level
+
+    discordTag = request.form['discordtag'] # requesting data input in the form asking for Discord Tag
+
+    email = request.form['email'] # requesting data input in the form asking for LPHS email address
     
-    data = name + ", " + info
+    data = name + ", " + year + ", " + discordTag + ", " + email # adding all of the data together to one variable called data
+ 
+    print(data) # printing variable data to the console for easy testing and debugging
 
-    print(data)
+    link = "https://discord.com/api/webhooks/824475546662600725/O7Ojqokl196NnnR6Hjs_---PEBAH_eEG4621WE6ieGKIpWND9PgW2xFFlEGRtVLx2gJC" # creating a seperate variable for the webhook link 
+ 
+    webhook = DiscordWebhook(url=link, content=data) # defining the webhook using the discord_webhook library, link variable and data variable
 
-    link = "https://discord.com/api/webhooks/824475546662600725/O7Ojqokl196NnnR6Hjs_---PEBAH_eEG4621WE6ieGKIpWND9PgW2xFFlEGRtVLx2gJC"
+    response = webhook.execute() # executing the webhook with message and url
 
-    webhook = DiscordWebhook(url=link, content=data)
-    response = webhook.execute()
-
-    # Notifier = dn.notifier(url)
-    # Notifier.send(data, print_message=true)
     
   return "Thank You For Entering Data"  # returning some feedback to the user after they submit the data
     
