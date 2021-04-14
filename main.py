ffa88f47c2e6d9591f46ccaae25d941280dfd6cf
BakedBeanLol <>
2021-04-14 01:37:37 +0000

    Added a template file for when user submits, added image for browser icons,

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def say():  # Function for when a post request is sent
  
     print(data) # printing variable data to the console for easy testing and debugging
 
-    link = "https://discord.com/api/webhooks/823821827285057576/oS3Y48XrLhjFOQ4H9ZWv4TLtlZoH2V-Lc1hb7clXwOZcPURc_30NHJVJcHDnhMc2dSPG" # creating a seperate variable for the webhook link 
+    link = "https://discord.com/api/webhooks/831442036930510848/2db9BqwlymYOQOTIEemUtMytaLnnZkmLTMyRhTR4eX0SLOdnyhdUb_2AhfK-8Ju_wsBx" # creating a seperate variable for the webhook link 
  
     webhook = DiscordWebhook(url=link, content=data) # defining the webhook using the discord_webhook library, link variable and data variable
 
@@ -34,10 +34,33 @@ def say():  # Function for when a post request is sent
     
   return render_template("theysentdata.html")  # returning some feedback to the user after they submit the data
     
+@app.route('/minecraft', methods=["POST"]) # Defining where our site will run if someone sends a post request
+def do():  # Function for when a post request is sent
+  if request.method == "POST":   
+    print("Someone wants to play Minecraft!") # logging "someone wants to play minecraft" in the console
+    mojanguser = request.form['mojanguser'] # requesting data input in the form asking for mojang username
+
+    discorduser = request.form['discorduser'] # requesting data input in the form asking for discord username
+
+    emaillphs = request.form['email2'] # requesting data input in the form asking for LPHS email address
+    
+    data2 = mojanguser + ", " + discorduser + ", " + emaillphs + ", "  # adding all of the data together to one variable called data, called data2 because i didnt want confusion between the two separate app routes
+ 
+    print(data2) # printing variable data to the console for easy testing and debugging
+
+    link = "https://discord.com/api/webhooks/831442441713483797/MDxhAibBKx6PJ1ggAxgrHz8qfXsCgfipkCUd-ywDpsdicW0I7qB8tqGmJlF7oHY8Vyc0" # creating a seperate variable for the webhook link 
+ 
+    webhook = DiscordWebhook(url=link, content=data2) # defining the webhook using the discord_webhook library, link variable and data variable
+
+    response = webhook.execute() # executing the webhook with message and url
+
+    print(response)
+    
+  return render_template("theysentdata.html")  # returning some feedback to the user after they submit the data
 
 @app.route('/') # Defining where our site will run by default
 def home():
-  return "You're not meant to be"
+  return "You're not meant to be here"
 
 if __name__ == "__main__": # Makes sure this is the main process
   app.run( # Start the site
